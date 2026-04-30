7d389afaae310088fd7fce1c342db32b6e31390c
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 11:28:48 -0300

    Estruturando p padrão MVVM e criando a classe RelayCommand no WPF

---
+++ InterfaceDeUsuarios/Data/UsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDeUsuarios.Data
+{
+    public class UsuarioRepository
+    {
+    }
+}
