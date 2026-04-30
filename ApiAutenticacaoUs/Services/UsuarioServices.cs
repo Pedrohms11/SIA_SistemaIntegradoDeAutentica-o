36089720eb3cde1548bb83ddf1bd35a5c7d41c19
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 09:31:38 -0300

    Implementada a classe usuario Service

--- ApiAutenticacaoUs/Services/UsuarioServices.cs
+++ ApiAutenticacaoUs/Services/UsuarioServices.cs
@@ -1,6 +1,26 @@
-﻿namespace ApiAutenticacaoUs.Services
+﻿using ApiAutenticacaoUs.Repositories.Interfaces;
+using SIA_SistemaIntegradoDeAutenticação;
+
+namespace ApiAutenticacaoUs.Services
 {
     public class UsuarioServices
     {
+        private readonly IUsuarioRepository _repo;
+
+        public UsuarioServices(IUsuarioRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<Usuarios>> Listar() => await _repo.GetAll();
+
+        public async Task<Usuarios> ObterPorId(int id) => await _repo.GetById(id);
+
+        public async Task Criar(Usuarios usuario) => await _repo.Add(usuario);
+
+        public async Task Atualizar(Usuarios usuario) => await _repo.Update(usuario);
+
+        public async Task Deletar(int id) => await _repo.Delete(id);
+
     }
 }
