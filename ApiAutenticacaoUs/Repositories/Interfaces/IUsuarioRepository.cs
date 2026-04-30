aa3bf82e903dab90dff20612c1dc2ae41200c40d
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 09:17:40 -0300

    Implementado a classe IUsuarioRepository

--- ApiAutenticacaoUs/Repositories/Interfaces/IUsuarioRepository.cs
+++ ApiAutenticacaoUs/Repositories/Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ApiAutenticacaoUs.Repositories.Interfaces
+﻿using SIA_SistemaIntegradoDeAutenticação;
+
+namespace ApiAutenticacaoUs.Repositories.Interfaces
 {
     public interface IUsuarioRepository
     {
+        Task<List<Usuarios>> GetAll();
+        Task<Usuarios> GetById(int id);
+        Task Add(Usuarios usuarios);
+        Task Update(Usuarios usuarios);
+        Task Delete(int id);
     }
 }
