58d31680088d0ba0ef5a034220be166a6ca126fb
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 15:04:22 -0300

    Implementada a classe Base view models responsaveil por iomplementar  a notificação de alteraçõpes na view

---
+++ InterfaceDeUsuarios/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDeUsuarios.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
