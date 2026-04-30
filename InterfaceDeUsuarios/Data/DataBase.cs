54f8716f9f09777fe01ad7bf56d95b1928ebe58c
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 12:01:58 -0300

    Implementada a classe Database

--- InterfaceDeUsuarios/Data/DataBase.cs
+++ InterfaceDeUsuarios/Data/DataBase.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace InterfaceDeUsuarios.Data
 {
     public class DataBase
     {
+        private static readonly string pastaBase =
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "BibliotecaComunitaria"
+            );
+
+        private static readonly string caminhoBanco =
+            Path.Combine(pastaBase, "bibliotecaCom.db");
+
+        private static readonly string connectionString =
+            $"Data Source={caminhoBanco}";
+
+         static DataBase()
+        {
+            if (!Directory.Exists(pastaBase))
+                Directory.CreateDirectory(pastaBase);
+
+            if (!File.Exists(caminhoBanco))
+                MessageBox.Show("Banco de dados inexistente !!!");
+        }
+
+        public static SqliteConnection GetConnection()
+        {
+            return new SqliteConnection(connectionString);
+        }
     }
 }
+
