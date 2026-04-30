e5e2fb9a0e58ab1948877f0ccf08ef8fefa3cc10
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 15:58:12 -0300

    Atualizado o arquivo app.xaml

--- InterfaceDeUsuarios/App.xaml.cs
+++ InterfaceDeUsuarios/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Configuration;
+﻿using InterfaceDeUsuarios.Data;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace InterfaceDeUsuarios
@@ -9,6 +11,35 @@ namespace InterfaceDeUsuarios
     /// </summary>
     public partial class App : Application
     {
+        public static IConfiguration Configuration { get; private set; }
+        public static ServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Configurar Configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            // Configurar DI
+            var services = new ServiceCollection();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            // Aplicar migrações
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+        }
+
     }
 
 }
