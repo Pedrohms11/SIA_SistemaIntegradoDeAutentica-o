0ce526abf27c6f907edda694cbb59c454b3d43ee
Pedro Moura <Pedromoura32@gmail.com>
2026-04-30 10:08:04 -0300

    Inserido a string de conexão no arquivo program.cs

--- ApiAutenticacaoUs/Program.cs
+++ ApiAutenticacaoUs/Program.cs
@@ -1,10 +1,15 @@
+using ApiAutenticacaoUs.Data;
 using ApiAutenticacaoUs.Repositories;
 using ApiAutenticacaoUs.Repositories.Interfaces;
 using ApiAutenticacaoUs.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
