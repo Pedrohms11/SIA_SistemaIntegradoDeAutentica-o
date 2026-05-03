4fbc97ec8839d402a719d30e4de16a133f4b32ae
Pedro Moura <Pedromoura32@gmail.com>
2026-05-03 16:15:31 -0300

    Criando arquivo DBcontext

---
+++ ConsoleMonitor/Services/MonitorDbContext.cs
@@ -0,0 +1,32 @@
+﻿using SIA_SistemaIntegradoDeAutenticação;
+
+
+namespace ConsoleMonitor.Services
+{
+    public class MonitorDbContext : DbContext
+    {
+        public MonitorDbContext(DbContextOptions<MonitorDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<HistoricoAlteracao> HistoricoAlteracoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configurar índices para melhor performance
+            modelBuilder.Entity<Usuario>()
+                .HasIndex(u => u.UltimaModificacao);
+
+            modelBuilder.Entity<HistoricoAlteracao>()
+                .HasIndex(h => h.DataHora);
+
+            modelBuilder.Entity<HistoricoAlteracao>()
+                .HasIndex(h => new { h.UsuarioId, h.DataHora });
+        }
+    }
+
+}
+}
